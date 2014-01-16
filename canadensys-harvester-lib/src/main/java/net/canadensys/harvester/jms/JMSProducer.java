d078b558e5b6a936972619b151b187e1aa887256
Christian Gendreau <christiangendreau@gmail.com>
2014-01-16 11:57:10 -0500

    fixed registeredMsgHandlers creation issue (prematurely merged from dev)

--- canadensys-harvester-lib/src/main/java/net/canadensys/harvester/jms/JMSProducer.java
+++ canadensys-harvester-lib/src/main/java/net/canadensys/harvester/jms/JMSProducer.java
@@ -25,56 +25,33 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Java Messaging System message producer
+ * 
  * @author canadensys
- *
+ * 
  */
-public class JMSProducer{
+public class JMSProducer {
 
 	private static final Logger LOGGER = Logger.getLogger(JMSProducer.class);
-	
-	private String brokerURL;
-	
+
+	private final String brokerURL;
+
 	// Name of the queue we will sent messages into
 	private static String QUEUE_NAME = "Importer.Queue";
 	private static String CONTROL_TOPIC = "Importer.Topic.Control";
-	
+
 	private Connection connection;
 	private Session session;
 	private MessageProducer producer;
-	
-	//Topic connection is used to public control commands
+
+	// Topic connection is used to public control commands
 	private TopicConnection topicConnection;
 	private TopicPublisher publisher;
-	
-	//Jackson Mapper to write Java object into JSON
-	private ObjectMapper om;
-	
-	public JMSProducer(String brokerURL){
-		this.brokerURL = brokerURL;
-	}
 
-	public void init() {
-		om = new ObjectMapper();
-		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(brokerURL);
-		
-		try{
-			// Getting JMS connection from the server and starting it
-			connection = factory.createConnection();
-			connection.start();
-			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-	
-			Destination destination = session.createQueue(QUEUE_NAME);
-			producer = session.createProducer(destination);
-			
-			//Topic
-			topicConnection = factory.createTopicConnection();
-			topicConnection.start();
-			TopicSession topicSession = topicConnection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
-			publisher = topicSession.createPublisher(topicSession.createTopic(CONTROL_TOPIC));
-		}
-		catch(JMSException jEx){
-			LOGGER.fatal("Can not initialize JMSProducer", jEx);
-		}
+	// Jackson Mapper to write Java object into JSON
+	private ObjectMapper om;
+
+	public JMSProducer(String brokerURL) {
+		this.brokerURL = brokerURL;
 	}
 
 	public void close() {
@@ -87,41 +64,72 @@ public class JMSProducer{
 			e.printStackTrace();
 		}
 	}
-	
+
+	public void init() {
+		om = new ObjectMapper();
+		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(
+				brokerURL);
+
+		try {
+			// Getting JMS connection from the server and starting it
+			connection = factory.createConnection();
+			connection.start();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+			Destination destination = session.createQueue(QUEUE_NAME);
+			producer = session.createProducer(destination);
+
+			// Topic
+			topicConnection = factory.createTopicConnection();
+			topicConnection.start();
+			TopicSession topicSession = topicConnection.createTopicSession(
+					false, Session.AUTO_ACKNOWLEDGE);
+			publisher = topicSession.createPublisher(topicSession
+					.createTopic(CONTROL_TOPIC));
+		} catch (JMSException jEx) {
+			LOGGER.fatal("Can not initialize JMSProducer", jEx);
+		}
+	}
+
+	/**
+	 * Publish a message to the broker. Published message will be read by all
+	 * consumer of the topic.
+	 * 
+	 * @param control
+	 */
+	public void publish(ControlMessageIF controlMsg) {
+		TextMessage message;
+		try {
+			message = session.createTextMessage(om
+					.writeValueAsString(controlMsg));
+			publisher.publish(message);
+		} catch (JMSException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Send message to the broker.
+	 * 
 	 * @param element
 	 */
 	public void send(ProcessingMessageIF element) {
 		TextMessage message;
 		try {
 			message = session.createTextMessage(om.writeValueAsString(element));
-			message.setStringProperty("MessageClass", element.getClass().getCanonicalName());
+			message.setStringProperty("MessageClass", element.getClass()
+					.getCanonicalName());
 			producer.send(message);
 		} catch (JMSException e) {
+			LOGGER.fatal("Can not send message", e);
 			e.printStackTrace();
 		} catch (JsonGenerationException e) {
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Publish a message to the broker.
-	 * Published message will be read by all consumer of the topic.
-	 * @param control
-	 */
-	public void publish(ControlMessageIF controlMsg){
-		TextMessage message;
-		try {
-			message = session.createTextMessage(om.writeValueAsString(controlMsg));
-			publisher.publish(message);
-		} catch (JMSException e) {
-			e.printStackTrace();
-		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
 	}
--- canadensys-harvester-ui/src/main/java/net/canadensys/harvester/main/ProcessingNodeMain.java
+++ canadensys-harvester-ui/src/main/java/net/canadensys/harvester/main/ProcessingNodeMain.java
@@ -1,5 +1,6 @@
 package net.canadensys.harvester.main;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.canadensys.harvester.ProcessingStepIF;
@@ -15,97 +16,118 @@ import org.springframework.stereotype.Component;
 
 /**
  * Processing node main class.
+ * 
  * @author canadensys
- *
+ * 
  */
 @Component
 public class ProcessingNodeMain {
-	//TODO move port to config
+	// TODO move port to config
 	private static final String IP = "tcp://%s:61616";
-	
+
 	@Autowired
 	private JMSConsumer jmsConsumer;
-	
+
 	@Autowired
 	@Qualifier("insertRawOccurrenceStep")
 	private ProcessingStepIF insertRawOccurrenceStep;
-	
+
 	@Autowired
 	private ProcessingStepIF processInsertOccurrenceStep;
-	
+
 	@Autowired
 	private ProcessingStepIF insertResourceContactStep;
-	
-	private List<ProcessingStepIF> registeredSteps;
-	private List<JMSConsumerMessageHandler> registeredMsgHandlers;
-	
+
+	private final List<ProcessingStepIF> registeredSteps;
+	private final List<JMSConsumerMessageHandler> registeredMsgHandlers;
+
+	public ProcessingNodeMain() {
+		registeredMsgHandlers = new ArrayList<JMSConsumerMessageHandler>();
+		registeredSteps = new ArrayList<ProcessingStepIF>();
+	}
+
 	/**
 	 * Initiate the node and start listening for messages.
-	 * @param brokerURL a new broker URL or null to use the default one
-	 * @param additionalMessageHandler user defined message handler (optional)
+	 * 
+	 * @param brokerURL
+	 *            a new broker URL or null to use the default one
+	 * @param additionalMessageHandler
+	 *            user defined message handler (optional)
 	 */
-	public <T extends JMSConsumerMessageHandler & ProcessingStepIF> void initiate(String brokerURL, List<T> additionalMessageHandler){
-		//check if we need to set a new broker URL
-		if(StringUtils.isNotBlank(brokerURL)){
+	public <T extends JMSConsumerMessageHandler & ProcessingStepIF> void initiate(
+			String brokerURL, List<T> additionalMessageHandler) {
+		// check if we need to set a new broker URL
+		if (StringUtils.isNotBlank(brokerURL)) {
 			jmsConsumer.setBrokerURL(brokerURL);
 		}
-		
+
 		System.out.println("Broker location : " + jmsConsumer.getBrokerUrl());
-		
-		//Declare step handlers (maybe this should be configurable?)
-		registeredMsgHandlers.add((JMSConsumerMessageHandler)insertRawOccurrenceStep);
+
+		// Declare step handlers (maybe this should be configurable?)
+		registeredMsgHandlers
+				.add((JMSConsumerMessageHandler) insertRawOccurrenceStep);
 		registeredSteps.add(insertRawOccurrenceStep);
-		
-		registeredMsgHandlers.add((JMSConsumerMessageHandler)processInsertOccurrenceStep);
+
+		registeredMsgHandlers
+				.add((JMSConsumerMessageHandler) processInsertOccurrenceStep);
 		registeredSteps.add(processInsertOccurrenceStep);
-				
-		registeredMsgHandlers.add((JMSConsumerMessageHandler)insertResourceContactStep);
+
+		registeredMsgHandlers
+				.add((JMSConsumerMessageHandler) insertResourceContactStep);
 		registeredSteps.add(insertResourceContactStep);
-		
-		if(additionalMessageHandler != null){
+
+		if (additionalMessageHandler != null) {
 			registeredMsgHandlers.addAll(additionalMessageHandler);
 			registeredSteps.addAll(additionalMessageHandler);
 		}
-		
-		//Register all message handlers to the JMS consumer
-		for(JMSConsumerMessageHandler currMsgHandler : registeredMsgHandlers){
+
+		// Register all message handlers to the JMS consumer
+		for (JMSConsumerMessageHandler currMsgHandler : registeredMsgHandlers) {
 			jmsConsumer.registerHandler(currMsgHandler);
 		}
-		
-		//Then, init all the steps
-		for(ProcessingStepIF currStep : registeredSteps){
-			//due to the async behavior, we do not use any sharedParameters (at least for now)
+
+		// Then, init all the steps
+		for (ProcessingStepIF currStep : registeredSteps) {
+			// due to the async behavior, we do not use any sharedParameters (at
+			// least for now)
 			currStep.preStep(null);
 		}
-		
-		//TODO register postStep calls
-		
+
+		// TODO register postStep calls
+
 		jmsConsumer.open();
 	}
-	
+
 	/**
 	 * Processing node entry point
+	 * 
 	 * @param newBrokerIp
 	 */
 	public static void main(String newBrokerIp) {
 		String newBrokerUrl = null;
-		if(StringUtils.isNotBlank(newBrokerIp)){
+		if (StringUtils.isNotBlank(newBrokerIp)) {
 			newBrokerUrl = String.format(IP, newBrokerIp);
 		}
-		
-		//prepare to allow additional (user defined) messageHandler defined as step
+
+		// prepare to allow additional (user defined) messageHandler defined as
+		// step
 		List<MessageHandlerStep> additionalMessageHandler = null;
-		
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ProcessingNodeConfig.class);
-		ProcessingNodeMain processingNodeBean = ctx.getBean(ProcessingNodeMain.class);
+
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
+				ProcessingNodeConfig.class);
+		ProcessingNodeMain processingNodeBean = ctx
+				.getBean(ProcessingNodeMain.class);
 		processingNodeBean.initiate(newBrokerUrl, additionalMessageHandler);
 	}
-	
+
 	/**
-	 * Inner class to satisfy the 2 interfaces list of ProcessingNodeMain initiate method.
-	 * TODO: Not very elegant, maybe define a real interface.
+	 * Inner class to satisfy the 2 interfaces list of ProcessingNodeMain
+	 * initiate method. TODO: Not very elegant, maybe define a real interface.
+	 * 
 	 * @author canadensys
-	 *
+	 * 
 	 */
-	private interface MessageHandlerStep extends JMSConsumerMessageHandler,ProcessingStepIF{}
+	private interface MessageHandlerStep extends JMSConsumerMessageHandler,
+			ProcessingStepIF {
+	}
 }
