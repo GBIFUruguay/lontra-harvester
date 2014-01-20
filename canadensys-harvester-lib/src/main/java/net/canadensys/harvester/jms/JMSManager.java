2b39281675b43cec073a5d26284f6c99929f599f
Christian Gendreau <christiangendreau@gmail.com>
2014-01-20 09:16:25 -0500

    formatting

--- canadensys-harvester-lib/src/main/java/net/canadensys/harvester/jms/JMSManager.java
+++ canadensys-harvester-lib/src/main/java/net/canadensys/harvester/jms/JMSManager.java
@@ -4,58 +4,99 @@ import org.apache.activemq.broker.jmx.QueueViewMBean;
 import org.apache.activemq.web.RemoteJMXBrokerFacade;
 import org.apache.activemq.web.config.SystemPropertiesConfiguration;
 
+/**
+ * Manager for JMS Broker using JMX.
+ * 
+ * JMX must be enable on the ActiveMQ broker.
+ * 
+ * --activemq.xml--
+ * 
+ * <broker ... useJmx="true">
+ * 
+ * <managementContext> <managementContext connectorPort="2011"/>
+ * </managementContext>
+ * 
+ * --startup script--
+ * 
+ * ACTIVEMQ_SUNJMX_START=
+ * "$ACTIVEMQ_SUNJMX_START -Dcom.sun.management.jmxremote.ssl=false"
+ * ACTIVEMQ_SUNJMX_START=
+ * "$ACTIVEMQ_SUNJMX_START -Dcom.sun.management.jmxremote.password.file=${ACTIVEMQ_CONF}/jmx.password"
+ * ACTIVEMQ_SUNJMX_START=
+ * "$ACTIVEMQ_SUNJMX_START -Dcom.sun.management.jmxremote.access.file=${ACTIVEMQ_CONF}/jmx.access"
+ * 
+ * @author cgendreau
+ * 
+ */
 public class JMSManager {
 
 	private final RemoteJMXBrokerFacade facade;
 
-	public JMSManager() {
+	/**
+	 * 
+	 * @param jmxHost
+	 * @param jmxPort
+	 * @param jmxUser
+	 *            as defined in jmx.access on the JMS broker
+	 * @param jmxPassword
+	 *            as defined in jmx.password on the JMS broker
+	 */
+	public JMSManager(String jmxHost, int jmxPort, String jmxUser,
+			String jmxPassword) {
 		this.facade = new RemoteJMXBrokerFacade();
 
-		// System.setProperty("webconsole.jms.url", "tcp://localhost:61616");
 		System.setProperty("webconsole.jmx.url",
-				"service:jmx:rmi:///jndi/rmi://localhost:2011/jmxrmi");
-		System.setProperty("webconsole.jmx.user", "admin");
-		System.setProperty("webconsole.jmx.password", "activemq");
+				"service:jmx:rmi:///jndi/rmi://" + jmxHost + ":" + jmxPort
+						+ "/jmxrmi");
+		System.setProperty("webconsole.jmx.user", jmxUser);
+		System.setProperty("webconsole.jmx.password", jmxPassword);
 
 		SystemPropertiesConfiguration w = new SystemPropertiesConfiguration();
-		System.out.println(w.getJmxUrls().toArray()[0] + "," + w.getJmxUser()
-				+ "," + w.getJmxPassword());
 		facade.setConfiguration(w);
-		try {
-			System.out.println(facade.getBrokerAdmin());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
+	/**
+	 * Purge the queue.
+	 * 
+	 * @param queueName
+	 */
 	public void cleanUp(String queueName) {
 		try {
-			System.out.println(facade.getBrokerName());
 			QueueViewMBean queue = facade.getQueue(queueName);
 			if (queue != null) {
 				queue.purge();
 				long size = queue.getQueueSize();
 				if (size > 0)
-					throw new IllegalStateException("Fila " + queueName
-							+ " não foi limpa!");
+					throw new IllegalStateException("Queue " + queueName
+							+ " could not be purged.");
 			}
 		} catch (Exception e) {
 			throw new IllegalStateException(e);
 		}
 	}
 
+	/**
+	 * Get the number of element in the queue.
+	 * 
+	 * @param queueName
+	 * @return
+	 */
 	public long getQueueSize(String queueName) {
 		try {
 			QueueViewMBean queue = facade.getQueue(queueName);
-			return (queue != null ? queue.getQueueSize() : 0);
+			if (queue != null) {
+				return queue.getQueueSize();
+			}
+			// should we throw an exception?
+			return 0;
 		} catch (Exception e) {
 			throw new IllegalStateException(e);
 		}
 	}
 
 	public static void main(String[] args) {
-		JMSManager manager = new JMSManager();
+		JMSManager manager = new JMSManager("localhost", 2011, "admin",
+				"activemq");
 		manager.cleanUp("toto");
 	}
 
